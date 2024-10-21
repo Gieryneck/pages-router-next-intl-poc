e1977cc168a1cfc692699d4bb1bffee78dfb0b53
Tomasz Gieryn <tomasz.gieryn@sonalake.com>
2024-10-21 13:50:54 +0200

    Merge Next and NextIntl routers

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,19 +1,51 @@
-import { createLocalizedPathnamesNavigation } from 'next-intl/navigation';
-import { defineRouting } from 'next-intl/routing';
+import { createLocalizedPathnamesNavigation } from "next-intl/navigation";
+import { defineRouting } from "next-intl/routing";
+import { NextRouter, useRouter as useNextRouter } from "next/router";
 
 export const routing = defineRouting({
-  locales: ['en', 'de'],
-  defaultLocale: 'en',
+  locales: ["en", "de"],
+  defaultLocale: "en",
   pathnames: {
-    '/': '/',
-    '/about': {
-      en: '/about',
-      de: '/ueber-uns'
-    }
+    "/": "/",
+    "/about": {
+      en: "/about",
+      de: "/ueber-uns",
+    },
   },
   // 'never' does not work, 'as-needed' gives query params when switching back to default lang
-  localePrefix: 'as-needed'
+  localePrefix: "as-needed",
 });
 
-export const {Link, redirect, usePathname, useRouter} =
-  createLocalizedPathnamesNavigation(routing);
+const {
+  Link,
+  redirect,
+  usePathname,
+  useRouter: useNextIntlRouter,
+} = createLocalizedPathnamesNavigation(routing);
+
+type NextIntlRouter = ReturnType<typeof useNextIntlRouter>;
+type CombinedRouter = NextRouter & NextIntlRouter;
+
+type Router = {
+  [K in keyof CombinedRouter]: K extends keyof NextIntlRouter
+    ? NextIntlRouter[K]
+    : K extends keyof NextRouter
+    ? NextRouter[K]
+    : never;
+};
+
+/**
+ * Needs to be called within NextIntlClientProvider
+ *  */
+const useRouter = function useCombinedRouter(): Router {
+  const router = useNextRouter();
+  const nextIntlRouter = useNextIntlRouter();
+
+  return {
+    ...router,
+    ...nextIntlRouter,
+  };
+};
+
+export { Link, redirect, usePathname, useRouter };
+
--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -1,10 +1,17 @@
-import { Link } from "@/i18n/routing";
+import { Link, useRouter } from "@/i18n/routing";
 import { GetStaticPathsContext, GetStaticPropsContext } from "next";
 import { useLocale } from "next-intl";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const locale = useLocale();
+  const router = useRouter();
+
+  const [isRouterReady, setIsRouterReady] = useState(false);
+  useEffect(() => {
+    if (router.isReady) setIsRouterReady(true);
+  }, [router.isReady])
 
   return (
     <>
@@ -17,12 +24,10 @@ export default function Home() {
       <main>
         <p>ROUTE: Home</p>
         <p>LOCALE: {locale}</p>
+        {isRouterReady && <p>COMBINED ROUTER PATH: {router.asPath}</p>}
+
         <div>
-
-
-          <Link href="/about">
-            About us
-          </Link>
+          <Link href="/about">About us</Link>
           {/* <a href="/about" hrefLang={locale}>About us</a> */}
         </div>
         <div>
@@ -34,6 +39,9 @@ export default function Home() {
           <Link href="/" locale="de">
             Switch path to German
           </Link>
+        </div>
+        <div>
+          <button onClick={() => router.push("/about")}>Push '/about'</button>
         </div>
       </main>
     </>
